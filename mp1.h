b0c0ecf9f9424de7fbfe43425e0b4f99e845ed6b
Aaron Huang <aaronhuang@umail.ucsb.edu>
2020-05-10 05:52:32 -0700

    smoothing = 100

--- mp1.h
+++ mp1.h
@@ -22,7 +22,7 @@ class MP1
             double trainTime = 0;
             double trainAcc = 0;
 
-            double smoothing = 1.2;
+            double smoothing = 100;
 
         public:
             void read(std::string train, std::string test);
